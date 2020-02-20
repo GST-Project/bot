3d1055d70e1bc152250b27ff211ca49ef3e45193
Adrian Avendano <adrian.mont@gmail.com>
2020-02-20 15:47:32 +0400

    Update mybot.js
    Added comments

--- mybot.js
+++ mybot.js
@@ -22,12 +22,11 @@ bot.onText(/\/speakers (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
 
-	//calling other file
 	
 	if (resp){
-	
+		//Data Access object calls our backend database searching for users and other data
 		dataAccess.searchUser(resp, function dataDone(data){
-			
+			// this is the reponse send back to bot app
 			bot.sendMessage(chatId, data);	
 		}); 
 		
@@ -38,18 +37,18 @@ bot.onText(/\/speakers (.+)/, (msg, match) => {
 });
 
 // Listen for any kind of message. There are different kinds of
-// messages.
+// messages. This is where the bot initially gets called and the main function starts here
 bot.on('message', (msg) => {
 	const chatId = msg.chat.id;
 	console.log(msg_count);
 	console.log(msg);
-	
+	// this is the start function and the bot starts here!
 	if (msg.text.toString().toLowerCase() == "/start"){
 		console.log("execute operation "+msg_count );		
 		msg_count = msg.message_id;
 		bot.sendMessage(chatId, "hey "+msg.chat.first_name+"! Great to have you here. What's your email address?");	
 	} else {
-		//Email search funtion
+		//Email search funtion and this is the second response from the bot
 		if (msg.message_id == msg_count + 2){
 			if (validateEmail(msg.text.toString())){
 				dataAccess.searchByEmail(msg.text.toString(), function dataDone(data){
@@ -57,50 +56,54 @@ bot.on('message', (msg) => {
 					//bot.sendMessage(chatId, "Now, can you tell me some of the skills you have?");
 					console.log("email search");
 				});
-			} else {
+			} else {// in case no email found the bot responds the followinfg
 				msg_count = msg.message_id;
 				bot.sendMessage(chatId, "Sorry, I something is wrong with the email, can you try again?");
 			}
 		}
-		//Skills Function
+		//Skills Function and the 3rd response from the bot!
 		if (msg.message_id == msg_count + 4){
 			console.log("insert skills");
 			skills = msg.text.toString().toLowerCase();	
 			bot.sendMessage(chatId, "Now, can you tell where are you located?");
                 }
-		//Location Function
+		//Location Function and the 4rd response from the bot!
                 if (msg.message_id == msg_count + 6){
 			console.log("insert Location");
                 	bot.sendMessage(msg.chat.id, "Please tell me what would you like to do from the 2 options", {
-"reply_markup": {
+"reply_markup": {// Here 2 options are presented to the user for input to either "Join" or "Start" a project!
     "keyboard": [ ["Join a project"], ["Start a project"]]
     }
 });	
 		}
 
-		// Join a project
+		// Join a project selection was made by the user
 		if (msg.text.toString().toLowerCase() == "join a project"){
+			//search on out backend for all the projects we have. Right now we are retrieving all projects but we will
+			//need to search projects by location and/or region once we have more of them
 			dataAccess.matchProject(skills, function dataDone(data){
                 		bot.sendMessage(chatId, "I found "+data.length+" that matched your criteria:");
 				for (var i = 0; i < data.length;i++){
 					bot.sendMessage(chatId, "\nProject name: "+data[i].name+"\n\nThey Need "+data[i].need+"\n\n"+data[i].description+"\n\n"+"location "+data[i].location+"\n"+data[i].url+"\nProject Number:"+data[i].project_number);
 				}
-
+				//Tells the user what to do after the list of projects has been displayed in the bot app!
 				if (data.length > 0){
 					bot.sendMessage(chatId,"Great!", {"reply_markup":{"remove_keyboard": true}});
 					bot.sendMessage(chatId,"\n Now, can you please tell me which project you will like to join? Just give us their project number: \n XX,YZ");
 				}
                         });	
 		}
-		// Start a project 
+		// Start a project was selected by the user
 		if (msg.text.toString().toLowerCase() == "start a project"){
-
+			// TODO: Function here to add a project to our database
 
                 }
 	
-		//Add user to project
+		//Add me to project was selected by the user. The user has selected a specific project number and he/she needs to be added
+		//to our database backend!
 		if (msg.message_id == msg_count + 14){
-			  bot.sendMessage(chatId,"Great!, I will notify the project owners and they will reply back to you!");
+			//TODO: function to add user to a specific project!
+			 bot.sendMessage(chatId,"Great!, I will notify the project owners and they will reply back to you!");
 			console.log("JOIN PROJECT")
 		}		
 		
@@ -110,6 +113,7 @@ bot.on('message', (msg) => {
   	//bot.sendMessage(chatId, "Hello "+msg.chat.first_name+" "+', welcome to our GST community. Here I can help you connect with the right people and work on projects!');
 });
 
+//Function to validate email format!
 function validateEmail(email) 
 {
     var re = /\S+@\S+\.\S+/;
